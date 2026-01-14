36423928b0b2c320de7c209ee750adbe2934f2c0
Twopoint Development® <124464772+joetwopoint@users.noreply.github.com>
2026-01-14 16:49:45 -0700

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,22 +1,22 @@
 Config = {
-	Guild_ID = '', -- Set to the ID of your guild (or your Primary guild if using Multiguild)
+	Guild_ID = 'GUILDIDHERE', -- Set to the ID of your guild (or your Primary guild if using Multiguild)
 	Multiguild = false, -- Set to true if you want to use multiple guilds
 	Guilds = {
-		["name"] = "guild_id", -- Replace this with a name, like "main"
+		["SERVERNAMEHERE"] = "GUILDIDHERE", -- Replace this with a name, like "main"
 	},
-	Bot_Token = '',
+	Bot_Token = 'BOTTOKEN',
 	RoleList = {},
 	DebugScript = false,
-	CacheDiscordRoles = true, -- true to cache player roles, false to make a new Discord Request every time
+	CacheDiscordRoles = false, -- true to cache player roles, false to make a new Discord Request every time
 	CacheDiscordRolesTime = 60, -- if CacheDiscordRoles is true, how long to cache roles before clearing (in seconds)
 }
 
 Config.Splash = {
-	Header_IMG = 'https://forum.cfx.re/uploads/default/original/3X/a/6/a6ad03c9fb60fa7888424e7c9389402846107c7e.png',
+	Header_IMG = 'IMGURLHERE',
 	Enabled = true,
-	Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
-	Heading1 = "Welcome to [ServerName]",
-	Heading2 = "Make sure to join our Discord and check out our website!",
-	Discord_Link = 'https://discord.gg',
-	Website_Link = 'https://badger.store',
+	Wait = 5, -- How many seconds should splash page be shown for? (Max is 12)
+	Heading1 = "Welcome",
+	Heading2 = "Whitelisted Click To Join The Discord",
+	Discord_Link = 'https://discord.gg/',
+	Website_Link = 'https://discord.gg/',
 }
